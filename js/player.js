adc9940523bf11bef9a9d7f924fb0317428330ff
Justin Thibault <justin@freshout.us>
2009-08-06 21:08:24 -0400

    Refactoring, added clickable timeline

--- js/player.js
+++ js/player.js
@@ -34,6 +34,8 @@ var frame_regex = RegExp("-[\\d]+\\.png$","g")
 var current_frame = 0;
 var frames;
 var stepper, playing;
+var tick_border = 0;
+var timeline_width = 300;
 
 
 function getImages(){
@@ -42,7 +44,7 @@ function getImages(){
 
 function getFPS(){
 	var input_fps, fps
-	input_fps = parseInt(document.getElementById("speed").value)
+	input_fps = parseFloat(document.getElementById("speed").value)
 	if(input_fps <= 0)
 	  input_fps = 0.1
 	return 1000/input_fps
@@ -59,6 +61,18 @@ function setPlayState(currently_playing){
 	}
 }
 
+function frameToTick(frame){
+	return parseInt((timeline_width/frames.length) * frame)
+}
+
+function tickToFrame(frame){
+	return parseInt((frames.length/timeline_width) * frame)
+}
+
+function markerWidth(frame){
+	return (parseInt(timeline_width/frames.length) + 1)
+}
+
 function replaceImage(pause){
 	pause = pause || false
 	if(current_frame >= frames.length)
@@ -67,6 +81,9 @@ function replaceImage(pause){
 	  current_frame = frames.length - 1
 	document.getElementById("frame_permalink").value = document.getElementById("viewer").getElementsByTagName("img")[0].src = frames[current_frame]
 	document.getElementById("current_frame").value = current_frame + 1
+	//document.getElementById("time_line").getElementsByTagName("div").each(function(){this.style.background = "#fff"})
+	//document.getElementById("time_line").getElementsByTagName("div")[frameToTick(current_frame)].style.background = "red"
+	document.getElementById("marker").style.left = frameToTick(current_frame) + "px"
 	current_frame++;
 	clearTimeout(stepper)
 	if(!pause)
@@ -99,7 +116,7 @@ function toggle_play(){
 }
 
 function createViewer(frames, element){
-	var viewer, stop, start, next, prev, speed, current, permalink, speed_label, current_label
+	var viewer, stop, start, next, prev, speed, current, permalink, speed_label, current_label, time_line
 	viewer          = document.createElement("div")
 	nextFrame       = document.createElement('img')
 	controls        = document.createElement("div")
@@ -114,6 +131,17 @@ function createViewer(frames, element){
 	permalink       = document.createElement("input")
 	speed_label     = document.createElement("label")
 	current_label   = document.createElement("label")
+	time_line       = document.createElement("div")
+	
+	var i = 0;
+	time_line.id = "time_line"
+	time_line.style.width = "300px"
+	time_line.style.margin = "0"
+	time_line.style.height = "10px"
+	time_line.style.cssFloat = "left"
+	time_line.style.styleFloat = "left"
+	time_line.style.position = "relative"
+	
 	controls.id = "controls"
 	prev.type = start.type = next.type = "button"
 	speed.type = current.type = permalink.type ="text"
@@ -144,6 +172,9 @@ function createViewer(frames, element){
 	viewer.style.padding = "10px"
 	viewer.className+=viewer.className?' box':'box';
 	
+	controls.style.clear = "both"
+	controls.style.cssFloat = "left"
+	
 	permalink.onclick = function(){ this.select() }
 	start.onclick     = function(){ toggle_play() }
 	next.onclick      = function(){ advance() }
@@ -166,6 +197,7 @@ function createViewer(frames, element){
 	controls.appendChild(current_label)
 	controls.appendChild(permalink_form)
 	viewer.appendChild(nextFrame)
+	viewer.appendChild(time_line)
 	viewer.appendChild(controls)
 	viewer.id = "viewer"
 	element = element || document.body
@@ -173,7 +205,47 @@ function createViewer(frames, element){
 
 	viewer.getElementsByTagName("label").each(function(){ this.style.padding = "3px" })
 	viewer.getElementsByTagName("input").grep(function(){return this.has_property("type",/text/)}).each(function(){ this.style.textAlign = "center";this.style.margin = "0 0 0 5px" })
-	viewer.getElementsByTagName("div").each(function()  { this.style.margin = "10px 0 0 0" })
+	viewer.getElementsByTagName("div").each(function()  { this.style.margin = "10px 0 0 0";this.style.textAlign="center" })
+	
+	//frames.each(function(){
+	var ticks = parseInt(time_line.style.width.replace(/[^\d]+/,''))
+	
+	for(var j=0;j < ticks;j++){
+		var f = document.createElement("div")
+		f.id = "f_" + j
+		f.className = "tick"
+		f.style.cssFloat = "left"
+		f.style.styleFloat = "left"
+		f.style.fontSize = "0.001px"
+		//f.style.width = parseInt((parseInt(time_line.style.width) / frames.length - tick_border)) + "px"
+		f.style.width = "1px"
+		f.style.height = "10px"
+		f.style.background = "#fff"
+		f.style.borderWidth = "0 " + tick_border + "px 0 0"
+		f.style.borderStyle = "solid"
+		f.style.borderColor = "#eee"
+		f.style.margin = "0"
+		time_line.appendChild(f)
+	}
+	
+	var marker = document.createElement("div")
+	marker.id = "marker"
+	marker.style.position = "absolute"
+	marker.style.top = "0px"
+	marker.style.left = "0px"
+	marker.style.width = "1px"
+	marker.style.height = "10px"
+	marker.style.background = "red"
+	marker.style.width = markerWidth() + "px"
+	time_line.appendChild(marker)
+	
+	time_line.getElementsByTagName("div").each(function(){
+		this.onclick = function(){
+			var this_frame = parseInt(this.id.replace(/[^\d]+/,''))
+			current_frame = tickToFrame(this_frame)
+			replaceImage(!playing)
+		}
+	})
 	
 	play()
 }
